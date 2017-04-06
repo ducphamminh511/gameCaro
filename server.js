e0d2b330ec0a78e79b36d33affd0166de00d8656
huy nguyen <huy.nguyen22994@gmail.com>
2017-04-06 21:09:54 +0700

    Update code + live chat

--- server.js
+++ server.js
@@ -14,13 +14,39 @@ app.get('/', function(req,res){
 })
 
 server.listen(app.get('port'),function(){
-    console.log('Dang chay o cong 5k :)');
+    console.log('Server is running on port : ' + app.get('port'));
 });
 
-io.on('connection', function(socket){
-    socket.on('clientClick',function(data){
+// Tạo đối tượng người chơi
+function player(name, turn) {
+    this.name = name;
+    this.id = "1";
+    this.turn = turn || null;
+};
+
+// Mãng người chơi
+var Players = [];
+
+io.on('connection', function (socket) {
+    socket.broadcast.emit('listPlayers', Players);
+    socket.on('clientClick', function (data) {
         console.log(data);
         socket.broadcast.emit('serverNotify', data);
+    });
+    socket.on('joinUser', function (data) {
+        // console.log(data);
+        // Khởi tạo đối tượng
+        var user = new player(data.username); 
+        Players.push(user);
+        //console.log(Players);
+        socket.broadcast.emit('listPlayers', Players);
+    });
+    socket.on('refresh', function (data) {
+        socket.emit('listPlayers', Players);
+    });
+    socket.on('sendMessage', function (data) {
+        //console.log(data);
+        socket.broadcast.emit('serverSendMsg', data);
     })
 });
 
