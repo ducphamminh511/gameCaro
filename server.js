2bcac46703c77c613a915f6b08af4a34f928ac65
Minh Duc <phamminhduc0511@gmail.com>
2017-04-06 11:33:13 +0700

    second cmt

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+//Load 
+
+//Chạy server
+var server = require('http').createServer(app);
+//socket
+var io = require('socket.io')(server);
+app.set('port', process.env.PORT || 5000);
+
+app.use(express.static(__dirname +'/public'));
+app.get('/', function(req,res){
+    res.sendFile(__dirname +'/caro.html');
+})
+
+server.listen(app.get('port'),function(){
+    console.log('Dang chay o cong 5k :)');
+});
+
+io.on('connection', function(socket){
+    socket.on('clientClick',function(data){
+        console.log(data);
+        socket.broadcast.emit('serverNotify', data);
+    })
+});
+
